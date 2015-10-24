245a9ee4b6f196552fcce079bb927aa43b4947e1
Peter Hancock <peter.hancock@gmail.com>
2015-10-24 09:36:40 +0100

    fixed typo

--- nth-term.py
+++ nth-term.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 
 # nth term, e.g. 'n + 1'
@@ -10,7 +12,7 @@ def nth_term(n):
     #eval is sloooow
     return eval(nth)
 
-print "The first ", n, " terms of seqence '", nth, "' are:"
+print "The first ", n, " terms of sequence '", nth, "' are:"
 
 for i in range(n):
     print i + 1, " -> ", nth_term(i + 1)
